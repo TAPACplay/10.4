81ef07ee8f097d6a74c9382badb68cd85d650452
TAPACplay <118310674+TAPACplay@users.noreply.github.com>
2022-12-08 10:04:41 +0200

    Add project files.

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	double y, x, ct;
+	setlocale(0, ".1251");
+	int a = 8.8;
+	int t;
+	int c;
+	cout << "Згачення змінної t \n";
+	cin >> t;
+	c= t*t + (sqrt (a));
+	x = log10(abs(c * t));
+	y = sin(3 * x) + cos(2 * x);
+	cout << "y = " << y;
+	
+	
+
+}
